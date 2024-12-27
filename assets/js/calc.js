191e3f754fb628d724286e88d14ede4a48a6f27a
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2024-12-27 16:39:59 -0300

    update fix

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -46,3 +46,13 @@ function removeBanner() {
         banner.style.display = 'none';
     }
 }
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
+        event.preventDefault(); // Impede o menu de contexto de aparecer
+    }
+});
+
+document.addEventListener('contextmenu', function (event) {
+    event.preventDefault(); // Impede o menu de contexto de aparecer
+});
